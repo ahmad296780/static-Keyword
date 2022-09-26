bc5beed0d3d714dc9d5f705b6dcb703f92957b79
ahmad <bha296780@gmail.com>
2022-09-26 11:32:31 -0400

    final

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using Static_Exercise;
+
+
+Console.WriteLine(TempConverter.CelsiusToFahrenheit(35));
+Console.WriteLine(TempConverter.FahrenheitToCelsius(95));
---
+++ TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Exercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double x)
+        {
+            return (x - 32) * 5 / 9;
+        }
+        public static double CelsiusToFahrenheit(double y)
+        {
+            return (y * 1.8) + 32;
+        }
+    }
+}
